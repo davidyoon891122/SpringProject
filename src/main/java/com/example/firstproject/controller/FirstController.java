9537f8196e5549bc5ce2b7fa199a9e72a0c9f31f
davidyoon <qqpo12@naver.com>
2022-08-02 20:38:06 +0900

    Add a bye request
    * Added a Model
    * Added a bye controller

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
-
     @GetMapping("/hi")
     public String niceToMeetYou(Model model) {
         model.addAttribute("username", "David Yoon");
         return "greetings"; // templates/greetings.mustache -> 브라우저로 전송!
+    }
+    @GetMapping("/bye")
+    public String seeYouAgain(Model model) {
+        model.addAttribute("nickname", "David Yoon");
+        return "goodbye";
     }
 }
