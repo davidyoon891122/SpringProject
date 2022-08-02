b406c8b3f855866d9a30fd86e2d33f58d93e5dce
davidyoon <qqpo12@naver.com>
2022-08-02 20:23:20 +0900

    Add a Controller
    * Added a Controller

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,13 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou() {
+        return "greetings"; // templates/greetings.mustache -> 브라우저로 전송!
+    }
+}
